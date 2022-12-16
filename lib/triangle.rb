ace77577a6bac3b41f37fd6bac9f577a7dccb24e
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-12-15 19:21:42 -0600

    commit tree 1b557638dbff83d9d4dfee7ecb2cc4f34d1821fc

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
+
+  attr_reader :side1, :side2, :side3
   # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    elsif side1 + side2 <= side3 || side3 + side1 <= side2 || side2 + side3 <= side1
+      raise TriangleError
+    elsif side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene#Ex:- :limit => 40
+    end
+  end
+  #Ex:- :default =>''
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
